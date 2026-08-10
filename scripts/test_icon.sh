7a332dc5473aaf87eeb26347ad791ceda4a07f40
Matias "Lechu" Siri <6060421+lechu77@users.noreply.github.com>
2026-08-10 17:18:39 +0200

    chore: remove unused scripts (process_nixie_images, set_theme, test_icon)

--- scripts/test_icon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -s "https://api.iconify.design/material-symbols/footprint.svg?color=white&width=256&height=256" -o /tmp/test.svg
-magick -background none -density 288 /tmp/test.svg -trim +repage -filter Lanczos -resize 30x30 -gravity center -extent 40x40 resources/drawables/icon_steps.png
